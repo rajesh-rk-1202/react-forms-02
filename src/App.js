d2d5bd190c308fd810717fcc9408d2047a5729d7
Rajesh Kumar Jena <jenarajeshkumar768@gmail.com>
2023-07-20 22:08:38 +0530

    added basic form with reusable components and dynamic values

--- src/App.js
+++ src/App.js
@@ -2,21 +2,83 @@ import React, { useState, useEffect, useRef } from "react";
 
 import "./App.css";
 import FormInput from "./components/FormInput";
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 
 function App() {
-  const [username, setUsername] = useState("");
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthdate: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username",
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email",
+    },
+    {
+      id: 3,
+      name: "birthdate",
+      type: "text",
+      placeholder: "Birthdate",
+      label: "Birthdate",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password",
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      label: "Confirm Password",
+    },
+  ];
 
   const submitHandler = (event) => {
     event.preventDefault();
+    setValues({
+      username: "",
+      email: "",
+      birthDay: "",
+      password: "",
+      confirmPassword: "",
+    });
+    console.log(values);
+  };
+
+  const onChangehandler = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
   };
 
   return (
     <div className="app">
       <form onSubmit={submitHandler}>
-        <FormInput name="username" placeholder="Username"></FormInput>
-        <FormInput name="email" placeholder="Email"></FormInput>
-        <FormInput name="fullname" placeholder="Full Name"></FormInput>
-        <FormInput name="sth" placeholder="Sth else"></FormInput>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChangehandler}
+          ></FormInput>
+        ))}
+
         <button>Submit</button>
       </form>
     </div>
--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -3,18 +3,13 @@ import React from "react";
 import classes from "./FormInput.module.css";
 
 const FormInput = (props) => {
-  const inputChangeHandler = (event) => {
-    console.log(props.placeholder);
-  };
+  const { label, onChange, id, ...inputProps } = props;
 
   return (
     <div className={classes.formInput}>
       {/* <label>User Name</label> */}
-      <input
-        name={props.name}
-        placeholder={props.placeholder}
-        onChange={inputChangeHandler}
-      ></input>
+      <label>{label}</label>
+      <input {...inputProps} onChange={onChange}></input>
     </div>
   );
 };
