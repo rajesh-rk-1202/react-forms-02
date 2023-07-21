5219d405f7e0bfc10fb28964d3193116dec272a1
Rajesh Kumar Jena <jenarajeshkumar768@gmail.com>
2023-07-21 17:06:05 +0530

    added some basic validation with html only

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -3,13 +3,15 @@ import React from "react";
 import classes from "./FormInput.module.css";
 
 const FormInput = (props) => {
-  const { label, onChange, id, ...inputProps } = props;
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  console.log(errorMessage);
 
   return (
     <div className={classes.formInput}>
       {/* <label>User Name</label> */}
       <label>{label}</label>
       <input {...inputProps} onChange={onChange}></input>
+      <span>{errorMessage}</span>
     </div>
   );
 };
